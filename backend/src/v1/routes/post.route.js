9815ea678e0c151d9b0ce34bbda047a95d39c33d
aryan <aaryan.p114@gmail.com>
2022-08-17 16:28:13 +0530

    bug fix

--- backend/src/v1/routes/post.route.js
+++ backend/src/v1/routes/post.route.js
@@ -31,7 +31,7 @@ router.post('/createpost', authCheck, async (req, res, next) => {
 router.post('/getpost', authCheck, async (req, res, next) => {
     if (req.user) {
         let posts = Post.find().populate('User_id LikedBy', 'Username Fullname Email ProfilePicture').sort({ Date: -1 })
-        return res.status(200).json({ err: null, posts })
+        // return res.status(200).json({ err: null, posts })
     };
 })
 router.post('/getprofile', authCheckBasic, async (req, res, next) => {
